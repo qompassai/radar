09a6530b251d473970edc8f67664b5ec00fde2ac
phaedrusflow <map@qompass.ai>
2025-05-25 13:26:23 -0700

    Update metadata, badge, and citation for 2025-05-25

--- examples/scripts/download_data.py
+++ examples/scripts/download_data.py
@@ -1,20 +1,7 @@
 #!/usr/bin/env python
-# This Software (Radar) is being made available as a public service by the
-# National Institute of Standards and Technology (NIST), an Agency of the United
-# States Department of Commerce. This software was developed in part by employees of
-# NIST and in part by NIST contractors. Copyright in portions of this software that
-# were developed by NIST contractors has been licensed or assigned to NIST. Pursuant
-# to Title 17 United States Code Section 105, works of NIST employees are not
-# subject to copyright protection in the United States. However, NIST may hold
-# international copyright in software created by its employees and domestic
-# copyright (or licensing rights) in portions of software that were assigned or
-# licensed to NIST. To the extent that NIST holds copyright in this software, it is
-# being made available under the Creative Commons Attribution 4.0 International
-# license (CC BY 4.0). The disclaimers of the CC BY 4.0 license apply to all parts
-# of the software developed or licensed by NIST.
-#
-# ACCESS THE FULL CC BY 4.0 LICENSE HERE:
-# https://creativecommons.org/licenses/by/4.0/legalcode
+# /qompassai/radar/examples/scripts/download_data.py
+# -----------------------------------
+# Copyright (C) 2025 Qompass AI, All rights reserved
 """Fetch and organize the datasets used in Radar's examples and demos.
 
 The Radar examples assume that the worker containers have access to datasets nested
@@ -143,9 +130,6 @@ from PIL import Image
 from rich.console import Console
 from upath import UPath
 
-# The try/except ImportError block allows this script to be invoked using:
-#     python ./scripts/download_data.py  # OR
-#     python -m scripts.download_data
 try:
     from .utils import RichConsole
 
